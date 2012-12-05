08c662a3664855d5df26151c43f9daa089b8b5ab
azurevn <azurevn@hotmail.com>
2012-12-05 22:52:56 +0700

    thêm isAdmin

--- ProjectA/Session.cs
+++ ProjectA/Session.cs
@@ -11,12 +11,20 @@ namespace DoAnB
         private static String Name;
         private static String username;
         private static String session;
+        private static bool admin;
 
-        public static void set(int Ma, String Ten, String TaiKhoan)
+        public static void set(int Ma, String Ten, String TaiKhoan, bool Admin)
         {
             ID = Ma;
             Name = Ten;
             username = TaiKhoan;
+            admin = Admin;
+            
+        }
+        public static bool _isAdmin
+        {
+            get { return admin; }
+            
         }
 
         public static int _EmployeeId
